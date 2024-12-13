cd49849a70c5033a9282f5c1af169b6c3a57dbf4
Roman Pashkov <roman.pashkov@capgemini.com>
2024-12-13 16:38:29 +0100

    Lab2

--- app/src/main/java/com/awstraining/backend/business/notifyme/adapter/TranslatorImpl.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/adapter/TranslatorImpl.java
@@ -14,30 +14,31 @@ import org.springframework.stereotype.Component;
 public class TranslatorImpl implements Translator {
 
     private static final Logger LOGGER = LogManager.getLogger(TranslatorImpl.class);
-    
 
-    
     // TODO: lab2
     //  1. Inject AWS AmazonTranslate from configuration TranslatorConfig.
     private final AmazonTranslate amazonTranslate;
+
     @Autowired
     public TranslatorImpl(AmazonTranslate amazonTranslate) {
 
         this.amazonTranslate = amazonTranslate;
     }
-    
+
     @Override
     public String translate(NotifyMeDO notifyMeDO) {
         // TODO: lab2
         //  1. Create translate text request.
         //  2. Call translate.
-        TranslateTextResult result = amazonTranslate
-            .translateText(new TranslateTextRequest().withText(notifyMeDO.text()))
-            .withSourceLanguageCode(notifyMeDO.sourceLc())
-            .withTargetLanguageCode(notifyMeDO.targetLc());
+        final TranslateTextRequest translateTextRequest = new TranslateTextRequest();
+        translateTextRequest.withText(notifyMeDO.text());
+        translateTextRequest.withSourceLanguageCode(notifyMeDO.sourceLc());
+        translateTextRequest.withTargetLanguageCode(notifyMeDO.targetLc());
+        TranslateTextResult translateTextResult = amazonTranslate.translateText(translateTextRequest);
+
         //  3. Log information about successful translated message.
-        LOGGER.info("Successfully translated text: " + notifyMeDO.text() + " to: " + result.getTargetLanguageCode());
+        LOGGER.info("Successfully translated text: " + notifyMeDO.text() + " to: " + translateTextResult.getTargetLanguageCode());
         //  4. Return translated message
-        return result.getTranslatedText();
+        return translateTextResult.getTranslatedText();
     }
 }
